0a362ae2fc63509ec453032cce9234f7999fd1e5
SHAO Ruizhao <shaorz@gmail.com>
2023-03-16 10:20:17 +0800

    Update client.py
    Static typing enhancement

--- easytrader/config/client.py
+++ easytrader/config/client.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-def create(broker):
+from dataclasses import dataclass
+from typing import Sequence, Mapping
+
+
+def create( broker: str ) -> CommonConfig:
     if broker == "yh":
         return YH
     if broker == "ht":
@@ -18,37 +22,37 @@ def create(broker):
         return UNIVERSAL
     raise NotImplementedError
 
-
+@dataclass ( frozen = True )
 class CommonConfig:
     DEFAULT_EXE_PATH: str = ""
-    TITLE = "网上股票交易系统5.0"
+    TITLE: str = "网上股票交易系统5.0"
 
     # 交易所类型。 深圳A股、上海A股
-    TRADE_STOCK_EXCHANGE_CONTROL_ID = 1003
+    TRADE_STOCK_EXCHANGE_CONTROL_ID: int = 1003
 
     # 撤销界面上， 全部撤销按钮
-    TRADE_CANCEL_ALL_ENTRUST_CONTROL_ID = 30001
+    TRADE_CANCEL_ALL_ENTRUST_CONTROL_ID: int = 30001
 
-    TRADE_SECURITY_CONTROL_ID = 1032
-    TRADE_PRICE_CONTROL_ID = 1033
-    TRADE_AMOUNT_CONTROL_ID = 1034
+    TRADE_SECURITY_CONTROL_ID: int = 1032
+    TRADE_PRICE_CONTROL_ID: int = 1033
+    TRADE_AMOUNT_CONTROL_ID: int = 1034
 
-    TRADE_SUBMIT_CONTROL_ID = 1006
+    TRADE_SUBMIT_CONTROL_ID: int = 1006
 
-    TRADE_MARKET_TYPE_CONTROL_ID = 1541
+    TRADE_MARKET_TYPE_CONTROL_ID: int = 1541
 
-    COMMON_GRID_CONTROL_ID = 1047
+    COMMON_GRID_CONTROL_ID: int = 1047
 
-    COMMON_GRID_LEFT_MARGIN = 10
-    COMMON_GRID_FIRST_ROW_HEIGHT = 30
-    COMMON_GRID_ROW_HEIGHT = 16
+    COMMON_GRID_LEFT_MARGIN: int = 10
+    COMMON_GRID_FIRST_ROW_HEIGHT: int = 30
+    COMMON_GRID_ROW_HEIGHT: int = 16
 
-    BALANCE_MENU_PATH = ["查询[F4]", "资金股票"]
-    POSITION_MENU_PATH = ["查询[F4]", "资金股票"]
-    TODAY_ENTRUSTS_MENU_PATH = ["查询[F4]", "当日委托"]
-    TODAY_TRADES_MENU_PATH = ["查询[F4]", "当日成交"]
+    BALANCE_MENU_PATH: Sequence[ str ] = ["查询[F4]", "资金股票"]
+    POSITION_MENU_PATH: Sequence[ str ] = ["查询[F4]", "资金股票"]
+    TODAY_ENTRUSTS_MENU_PATH: Sequence[ str ] = ["查询[F4]", "当日委托"]
+    TODAY_TRADES_MENU_PATH: Sequence[ str ] = ["查询[F4]", "当日成交"]
 
-    BALANCE_CONTROL_ID_GROUP = {
+    BALANCE_CONTROL_ID_GROUP: Mapping[ str, int ] = {
         "资金余额": 1012,
         "可用金额": 1016,
         "可取金额": 1017,
@@ -56,7 +60,7 @@ class CommonConfig:
         "总资产": 1015,
     }
 
-    POP_DIALOD_TITLE_CONTROL_ID = 1365
+    POP_DIALOD_TITLE_CONTROL_ID: int = 1365
 
     GRID_DTYPE = {
         "操作日期": str,
@@ -70,21 +74,22 @@ class CommonConfig:
         "发生日期": str,
     }
 
-    CANCEL_ENTRUST_ENTRUST_FIELD = "合同编号"
-    CANCEL_ENTRUST_GRID_LEFT_MARGIN = 50
-    CANCEL_ENTRUST_GRID_FIRST_ROW_HEIGHT = 30
-    CANCEL_ENTRUST_GRID_ROW_HEIGHT = 16
+    CANCEL_ENTRUST_ENTRUST_FIELD: str = "合同编号"
+    CANCEL_ENTRUST_GRID_LEFT_MARGIN: int = 50
+    CANCEL_ENTRUST_GRID_FIRST_ROW_HEIGHT: int = 30
+    CANCEL_ENTRUST_GRID_ROW_HEIGHT: int = 16
 
-    AUTO_IPO_SELECT_ALL_BUTTON_CONTROL_ID = 1098
-    AUTO_IPO_BUTTON_CONTROL_ID = 1006
-    AUTO_IPO_MENU_PATH = ["新股申购", "批量新股申购"]
-    AUTO_IPO_NUMBER = '申购数量'
+    AUTO_IPO_SELECT_ALL_BUTTON_CONTROL_ID: int = 1098
+    AUTO_IPO_BUTTON_CONTROL_ID: int = 1006
+    AUTO_IPO_MENU_PATH: Sequence[ str ] = ["新股申购", "批量新股申购"]
+    AUTO_IPO_NUMBER: str = '申购数量'
 
 
-class YH(CommonConfig):
-    DEFAULT_EXE_PATH = r"C:\双子星-中国银河证券\Binarystar.exe"
+@dataclass ( frozen = True )
+class YH( CommonConfig ):
+    DEFAULT_EXE_PATH: str = r"C:\双子星-中国银河证券\Binarystar.exe"
 
-    BALANCE_GRID_CONTROL_ID = 1308
+    BALANCE_GRID_CONTROL_ID: int = 1308
 
     GRID_DTYPE = {
         "操作日期": str,
@@ -98,13 +103,14 @@ class YH(CommonConfig):
         "发生日期": str,
     }
 
-    AUTO_IPO_MENU_PATH = ["新股申购", "一键打新"]
+    AUTO_IPO_MENU_PATH: Sequence[ str ] = ["新股申购", "一键打新"]
 
 
-class HT(CommonConfig):
-    DEFAULT_EXE_PATH = r"C:\htzqzyb2\xiadan.exe"
+@dataclass ( frozen = True )
+class HT( CommonConfig ):
+    DEFAULT_EXE_PATH: str = r"C:\htzqzyb2\xiadan.exe"
 
-    BALANCE_CONTROL_ID_GROUP = {
+    BALANCE_CONTROL_ID_GROUP: Mapping[ str, int ] = {
         "资金余额": 1012,
         "冻结资金": 1013,
         "可用金额": 1016,
@@ -125,29 +131,12 @@ class HT(CommonConfig):
         "发生日期": str,
     }
 
-    AUTO_IPO_MENU_PATH = ["新股申购", "批量新股申购"]
+    AUTO_IPO_MENU_PATH: Sequence[ str ] = ["新股申购", "批量新股申购"]
 
 
-class GJ(CommonConfig):
-    DEFAULT_EXE_PATH = "C:\\全能行证券交易终端\\xiadan.exe"
-
-    GRID_DTYPE = {
-        "操作日期": str,
-        "委托编号": str,
-        "申请编号": str,
-        "合同编号": str,
-        "证券代码": str,
-        "股东代码": str,
-        "资金帐号": str,
-        "资金帐户": str,
-        "发生日期": str,
-    }
-
-    AUTO_IPO_MENU_PATH = ["新股申购", "新股批量申购"]
-
-class GF(CommonConfig):
-    DEFAULT_EXE_PATH = "C:\\gfzqrzrq\\xiadan.exe"
-    TITLE = "核新网上交易系统"
+@dataclass ( frozen = True )
+class GJ( CommonConfig ):
+    DEFAULT_EXE_PATH: str = "C:\\全能行证券交易终端\\xiadan.exe"
 
     GRID_DTYPE = {
         "操作日期": str,
@@ -161,33 +150,57 @@ class GF(CommonConfig):
         "发生日期": str,
     }
 
-    AUTO_IPO_MENU_PATH = ["新股申购", "批量新股申购"]
+    AUTO_IPO_MENU_PATH: Sequence[ str ] = ["新股申购", "新股批量申购"]
 
-class WK(HT):
+
+@dataclass ( frozen = True )
+class GF( CommonConfig ):
+    DEFAULT_EXE_PATH: str = "C:\\gfzqrzrq\\xiadan.exe"
+    TITLE: str = "核新网上交易系统"
+
+    GRID_DTYPE = {
+        "操作日期": str,
+        "委托编号": str,
+        "申请编号": str,
+        "合同编号": str,
+        "证券代码": str,
+        "股东代码": str,
+        "资金帐号": str,
+        "资金帐户": str,
+        "发生日期": str,
+    }
+
+    AUTO_IPO_MENU_PATH: Sequence[ str ] = ["新股申购", "批量新股申购"]
+
+
+@dataclass ( frozen = True )
+class WK( HT ):
     pass
 
 
-class HTZQ(CommonConfig):
-    DEFAULT_EXE_PATH = r"c:\\海通证券委托\\xiadan.exe"
+@dataclass ( frozen = True )
+class HTZQ( CommonConfig ):
+    DEFAULT_EXE_PATH: str = r"c:\\海通证券委托\\xiadan.exe"
 
-    BALANCE_CONTROL_ID_GROUP = {
+    BALANCE_CONTROL_ID_GROUP: Mapping[ str, int ] = {
         "资金余额": 1012,
         "可用金额": 1016,
         "可取金额": 1017,
         "总资产": 1015,
     }
 
-    AUTO_IPO_NUMBER = '可申购数量'
+    AUTO_IPO_NUMBER: str = '可申购数量'
 
 
-class UNIVERSAL(CommonConfig):
-    DEFAULT_EXE_PATH = r"c:\\ths\\xiadan.exe"
+@dataclass ( frozen = True )
+class UNIVERSAL( CommonConfig ):
+    DEFAULT_EXE_PATH: str = r"c:\\ths\\xiadan.exe"
 
-    BALANCE_CONTROL_ID_GROUP = {
+    BALANCE_CONTROL_ID_GROUP: Mapping[ str, int ] = {
         "资金余额": 1012,
         "可用金额": 1016,
         "可取金额": 1017,
         "总资产": 1015,
     }
 
-    AUTO_IPO_NUMBER = '可申购数量'
+    AUTO_IPO_NUMBER: str = '可申购数量'
