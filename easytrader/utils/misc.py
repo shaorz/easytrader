17b33e434dd7e4edc0db712533766845f8b4f83f
SHAO Ruizhao <shaorz@gmail.com>
2023-03-16 10:31:21 +0800

    Update misc.py

--- easytrader/utils/misc.py
+++ easytrader/utils/misc.py
@@ -1,8 +1,9 @@
 # coding:utf-8
 import json
+from typing import Mapping
 
 
-def parse_cookies_str(cookies):
+def parse_cookies_str( cookies: str ):
     """
     parse cookies str to dict
     :param cookies: cookies str
@@ -10,22 +11,22 @@ def parse_cookies_str(cookies):
     :return: cookie dict
     :rtype: dict
     """
-    cookie_dict = {}
-    for record in cookies.split(";"):
-        key, value = record.strip().split("=", 1)
-        cookie_dict[key] = value
+    cookie_dict: Mapping = {}
+    for record in cookies.split( ";" ):
+        key, value = record.strip().split( "=", 1 )
+        cookie_dict[ key ] = value
     return cookie_dict
 
 
-def file2dict(path):
-    with open(path, encoding="utf-8") as f:
-        return json.load(f)
+def file2dict( path: str ) -> json:
+    with open( path, encoding="utf-8" ) as f:
+        return json.load( f )
 
 
-def grep_comma(num_str):
-    return num_str.replace(",", "")
+def grep_comma( num_str: str ) -> str:
+    return num_str.replace( ",", "" )
 
 
-def str2num(num_str, convert_type="float"):
-    num = float(grep_comma(num_str))
-    return num if convert_type == "float" else int(num)
+def str2num( num_str: str , convert_type: str = "float" ):
+    num = float( grep_comma( num_str ) )
+    return num if convert_type == "float" else int( num )
